0c3840ec0d553328a80adec6e109b00b5529eee2
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-27 18:38:12 +0100

    feat(component): creates the presentation layer

---
+++ src/context/auth/presentation/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, model } from '@angular/core';
+import { AuthError } from '@supabase/supabase-js';
+import { AuthFacade } from 'src/context/auth/application/facades/auth.facade';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	standalone: false,
+})
+export class LoginComponent {
+	username = model('');
+	password = model('');
+
+	constructor(private authFacade: AuthFacade) {}
+
+	onMakeLogin(): void {
+		this.login();
+	}
+
+	private async login() {
+		try {
+			await this.authFacade.login(this.username(), this.password());
+
+			console.log('Login successful');
+		} catch (error: AuthError | any) {
+			console.error(error.message);
+			return;
+		}
+	}
+}
