bfd5cb1cb7268b7948504f3c4f88d89d681911b4
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-27 18:37:30 +0100

    feat(module): create the module to make the di

---
+++ src/context/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { AuthPort } from './domain/port/auth.port';
+import { SupabaseAuthAdapter } from './infrastructure/supabase-auth.adater';
+import { LoginUseCase } from './application/login.use-case';
+import { AuthFacade } from './application/facades/auth.facade';
+import { AuthStatePort } from './domain/port/auth-state.port';
+import { NgxsAuthStateAdapter } from './infrastructure/state/ngxs-auth-state.adapter';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './presentation/components/login/login.component';
+import { TokenManager } from './infrastructure/token-manager';
+
+@NgModule({
+	providers: [
+		LoginUseCase,
+		AuthFacade,
+		{
+			provide: AuthPort,
+			useClass: SupabaseAuthAdapter,
+		},
+		{
+			provide: AuthStatePort,
+			useClass: NgxsAuthStateAdapter,
+		},
+		TokenManager,
+	],
+	imports: [FormsModule],
+	declarations: [LoginComponent],
+	exports: [LoginComponent],
+})
+export class AuthModule {}
