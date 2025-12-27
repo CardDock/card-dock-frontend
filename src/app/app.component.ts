3d6245cfe4664d2e4238c38437b1088f4b896e7d
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-27 18:38:51 +0100

    feat(app): orchestrate the auth module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthModule } from 'src/context/auth/auth.module';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, AuthModule],
 	templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngxs/store';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
 
 import { routes } from './app.routes';
+import { AuthState } from 'src/context/auth/infrastructure/state/auth.state';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+	providers: [
+		provideStore([AuthState], withNgxsLoggerPlugin()),
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+	],
 };
