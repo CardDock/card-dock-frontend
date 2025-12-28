de4e4698d4b9bd6739241749833ca60aefe6dc9f
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-28 16:21:44 +0100

    feat(facade): initialize through the facade

--- src/context/auth/application/facades/auth.facade.ts
+++ src/context/auth/application/facades/auth.facade.ts
@@ -3,6 +3,7 @@ import { LoginUseCase } from '../login.use-case';
 import { AuthUser } from '../../domain/user/user.entity';
 import { AuthStatePort } from '../../domain/port/auth-state.port';
 import { LogoutUseCase } from '../logout.use-case';
+import { InitializeAuthUseCase } from '../initialize-auth.use-case';
 
 @Injectable()
 export class AuthFacade {
@@ -14,6 +15,7 @@ export class AuthFacade {
 		private readonly loginUseCase: LoginUseCase,
 		private readonly logoutUseCase: LogoutUseCase,
 		private readonly authStatePort: AuthStatePort,
+		private readonly initializeAuthUseCase: InitializeAuthUseCase,
 	) {}
 
 	async login(email: string, password: string): Promise<AuthUser> {
@@ -31,5 +33,14 @@ export class AuthFacade {
 		await this.logoutUseCase.execute();
 
 		this.authStatePort.clearUser();
+	}
+
+	async initialize(): Promise<void> {
+		const result = await this.initializeAuthUseCase.execute();
+
+		if (result) {
+			const user = result;
+			this.authStatePort.setUser(user);
+		}
 	}
 }
