12d2b35d33d99ab92b3405c59c6bd4ed584277d1
Nelson R. Graña <nelsongraa8@gmail.com>
2025-12-29 02:11:22 +0100

    feat(domain): create action singup in domain port

--- src/context/auth/domain/port/auth.port.ts
+++ src/context/auth/domain/port/auth.port.ts
@@ -4,4 +4,5 @@ export abstract class AuthPort {
 	abstract login(email: string, password: string): Promise<AuthUser>;
 	abstract logout(): Promise<void>;
 	abstract getCurrentSession(): Promise<AuthUser | null>;
+	abstract singUp(email: string, password: string): Promise<AuthUser>;
 }
