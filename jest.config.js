b2be0706d2c8a95981be4fef49cfca0efd7d4fa6
Nelson R. Graña <nelsongraa8@gmail.com>
2026-01-11 13:02:24 +0100

    fix(test): corrects config file for testing

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,43 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // codigo antes de ejecutar cada test
-  moduleNameMapper: {
-    '@app/(.*)': '<rootDir>\\src\\app\\$1',
-    '@src/(.*)': '<rootDir>\\src\\$1'
-  },
-  testEnvironment: 'jest-environment-jsdom',
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/tests/coverage/',
-    '<rootDir>/tests/e2e/',
-    '<rootDir>/tests/performance/'
-  ],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/main.ts',
-    '!src/**/*.module.ts',
-    '!src/**/*.array.ts',
-    '!src/**/*.config.ts',
-    '!src/**/*.routes.ts',
-    '!src/environments/*.ts',
-    '!src/**/*.mock.ts'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  }
+	preset: 'jest-preset-angular',
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // codigo antes de ejecutar cada test
+	moduleDirectories: ['node_modules', '.pnpm'],
+	moduleNameMapper: {
+		'@app/(.*)': '<rootDir>/src/app/$1',
+		'@src/(.*)': '<rootDir>/src/$1',
+		'^src/(.*)$': '<rootDir>/src/$1',
+	},
+	testEnvironment: 'jest-environment-jsdom',
+	// testEnvironment: '@angular-devkit/build-angular/plugins/jest/jsdom-environment',
+	testPathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/dist/',
+		'<rootDir>/tests/coverage/',
+		'<rootDir>/tests/e2e/',
+		'<rootDir>/tests/performance/',
+	],
+	transformIgnorePatterns: [
+		'node_modules/(?!.*\\.mjs$|@angular|@ngrx|@ngx)'
+	],
+	cache: false,
+	cacheDirectory: '<rootDir>/.jest-cache',
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'src/**/*.ts',
+		'!src/main.ts',
+		'!src/**/*.module.ts',
+		'!src/**/*.array.ts',
+		'!src/**/*.config.ts',
+		'!src/**/*.routes.ts',
+		'!src/environments/*.ts',
+		'!src/**/*.mock.ts',
+	],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 };
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,8 @@
-import 'jest-preset-angular/setup-jest';
+import 'jest-preset-angular/setup-env/zone';
+import { TestBed } from '@angular/core/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+
+TestBed.initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting()
+);
